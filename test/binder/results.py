7443be8c5442dda129811b5ee0bf6d2fe36200ba
Luciano Bello <luciano.bello@ibm.com>
2020-06-03 14:46:19 -0400

    the first test

---
+++ test/binder/results.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2020.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+import os
+
+CWD = os.path.dirname(os.path.abspath(__file__))
+
+
+class Results:
+    def __init__(self, namelist):
+        self.namelist = namelist
+
+    def _repr_html_(self):
+        ret = "<table>"
+        ret += '<tr><td>Filename</td><td>Result</td><td>Reference</td></tr>'
+        for name in self.namelist:
+            ret += '<tr>'
+            ret += '<td>' + name + '</td>'
+            ret += '<td><img src="' + name + '"</td>'
+            reference = os.path.join('mpl', 'references', name)
+            if os.path.exists(os.path.join(CWD, reference)):
+                ret += '<td><img src="' + reference + '"</td>'
+            else:
+                ret += '<td>copy the reference file to the reference directory</td>'
+            ret += '</tr>'
+        ret += "</table>"
+        return ret
+
+
+namelist = []
+for file in os.listdir(CWD):
+    if file.endswith(".png"):
+        namelist.append(file)
+results = Results(namelist)
